6c5ba30fdaaf45cd138f05bac20f77479ea92046
Jinyuan Zhang <jinyuanzhang1992@hotmail.com>
2024-08-22 04:24:23 +0930

    Revert "Merge pull request #4 from alert-city/feat/CP-14-Registration-Page"
    This reverts commit c1c3a57468bd1ecce5426e0e93500cb993d63b8f, reversing
    changes made to 1102dfcf71c5ee0a604f28e9cf932092d11be0af.

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import RegisterForm  from '@/modules/auth/register/RegistrationPage';
 
 const RegisterPage: React.FC = () => {
   return (
     <div>
-      <RegisterForm />
+      <h1>Register Page</h1>
     </div>
   );
 };
--- src/shared/constants/storage.ts
+++ src/shared/constants/storage.ts
@@ -3,4 +3,3 @@ export const USERNAME = 'username';
 export const IS_STAY_SIGNED_IN = 'is_stay_signed_in';
 export const AUTH_STATUS = 'auth_status';
 export const LOGIN_INFO = 'login_info';
-export const TEMP_USERNAME = 'temp_username';
