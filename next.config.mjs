98e48bba6cf3760a5c5d44a6e7e858e1c44464b3
Jinyuan Zhang <jinyuanzhang1992@hotmail.com>
2024-09-09 00:41:23 +0930

    Feat: CI/CD
    Finalised following features:
    - Modified the mapping in docker-publish.yml
    
    Resolve CP-0

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,12 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // 禁用 ESLint 检查
   },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_REST_API_URL: process.env.NEXT_PUBLIC_REST_API_URL,
+    NEXT_PUBLIC_WEBSOCKET_URL: process.env.NEXT_PUBLIC_WEBSOCKET_URL,
+    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
+  },
 };
 
 // export default nextConfig;
