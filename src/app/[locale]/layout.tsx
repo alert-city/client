92a5bc9a9f39b995f6e7d86abff5a38e4463d691
Jinyuan Zhang <jinyuanzhang1992@hotmail.com>
2024-09-10 01:37:28 +0930

    Feat: PWA
    Finalised following features:
    - Added some icons and manifest.json into public file to fit PWA
    - Added some logics into app/[locale]/layout.tsx to set PWA
    - Changed the user registration logic
    - Changed the validation for firstname and lastname
    
    Resolve CP-35

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -14,6 +14,10 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Alert City',
   description: 'A CMS for managing campus events and activities including registration and ticketing.',
+  icons: {
+    icon: '/images/alertcity-dark.png',
+    apple: '/images/alertcity-IOS.png',
+  },
 };
 
 type RootLayoutProps = {
@@ -25,10 +29,15 @@ const RootLayout = async ({ children, params: { locale } }: RootLayoutProps) => 
   const messages = await getMessages();
   return (
     <html lang={locale}>
+    <head>
+      <title>Alert City</title>
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="theme-color" content="#ffffff" />
+    </head>
     <body className={inter.className}>
     <ProviderWrapper>
       <ThemeWrapper>
-        <NextIntlClientProvider locale={locale} messages={messages}>
+      <NextIntlClientProvider locale={locale} messages={messages}>
           {children}
         </NextIntlClientProvider>
       </ThemeWrapper>
