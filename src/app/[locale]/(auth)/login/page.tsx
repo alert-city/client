47a17d19078478fad2addef694e65f4ef5b9a751
Jinyuan Zhang <jinyuanzhang1992@hotmail.com>
2024-09-12 00:54:49 +0930

    Feat: Third-Party Login
    Finalised following features:
    - Added OAuthLogin method into auth.service.ts
    - Changed the response DTO of login method
    - Optimized the refresh token and access token at auth.service.ts
    - Added new fields googleId and facebookId into user schema
    - Changed the send email logic. Frontend need not pass emailInfoType to backend
    - Added conditional adjustment into createUser method
    - Changed the locale of send email from context to middleware
    
    Resolve CP-36

--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import LoginForm from '../../../../modules/auth/login/LoginForm';
 import LoginLayout from '@/app/[locale]/(auth)/login/layout';
 import { RouteConfig } from '@/routes/route';
+import { useTranslations } from 'next-intl';
 
 export const metadata = RouteConfig.Login.Metadata;
 
 const LoginPage: React.FC = () => {
+  const t = useTranslations('LoginPage');
   return (
     <LoginLayout>
       <LoginForm />
       <p className="text-center text-sm text-gray-600 mt-6">
-        By signing in, you agree to our{' '}
+        {t('signInAgreement')}{' '}
         <RouteConfig.PrivacyPolicy.Link className="text-blue-500 hover:underline">
-          Privacy Policy
+          { t('privacyPolicy')}
         </RouteConfig.PrivacyPolicy.Link>
       </p>
     </LoginLayout>
